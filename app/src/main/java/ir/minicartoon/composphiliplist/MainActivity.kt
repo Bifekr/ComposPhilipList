cb4854737d6ab52a93d44637692efb230185818a
miladj796 <miladjamshidpur@gmail.com>
2023-07-26 16:41:26 +0330

    finish lessen1,2

--- app/src/main/java/ir/minicartoon/composphiliplist/MainActivity.kt
+++ app/src/main/java/ir/minicartoon/composphiliplist/MainActivity.kt
@@ -1,23 +1,25 @@
 package ir.minicartoon.composphiliplist
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import ir.minicartoon.composphiliplist.ui.theme.ComposPhilipListTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +35,10 @@ class MainActivity : ComponentActivity() {
 
 }
 
+@Composable
+fun ProfileImage() {
+
+}
 
 data class Message(val author: String, val message: String)
 
@@ -43,8 +49,31 @@ fun NewMessageCard(msg: String) {
 
 @Composable
 fun MessageCard(message: Message) {
-    Text(text = message.author)
-    Text(text = message.message)
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = androidx.core.R.drawable.ic_call_answer),
+            contentDescription = "content Profile",
+            Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+
+        )
+        // Add a horizontal space between the image and the column
+        Spacer(
+            modifier = Modifier.width(8.dp)
+        )
+        Column {
+            Text(text = message.author)
+            // Add a vertical space between the author and message texts
+            Spacer(Modifier.height(12.dp))
+            Text(text = message.message)
+        }
+    }
+
+
 }
 
 @Preview
