64a8452f9c7e219ca4944e0ea9f09dca8d39758c
miladj796 <miladjamshidpur@gmail.com>
2023-07-28 17:45:57 +0330

    start Part5(stat)

--- app/src/main/java/ir/minicartoon/composphiliplist/playlist/TextStyle.kt
+++ app/src/main/java/ir/minicartoon/composphiliplist/playlist/TextStyle.kt
@@ -13,12 +13,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import ir.minicartoon.composphiliplist.R
@@ -43,14 +46,33 @@ class TextStyle : ComponentActivity() {
                     .fillMaxSize()
 
             ) {
-
                 Text(
-                    text = "Jetpack Compose",
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Magenta,
+                                fontSize = 60.sp
+                            )
+                        ) {
+                            append("J")
+                        }
+                        append("etpack ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Magenta,
+                                fontSize = 60.sp
+                            )
+                        ) {
+                            append("C")
+                        }
+                        append("ompose ")
+                    },
+//                    text = "Jetpack Compose",
                     color = Color.Green,
                     fontSize = 35.sp,
                     fontStyle = FontStyle.Italic,
                     fontFamily = fontFamily,
-                    fontWeight = FontWeight.Thin,
+                    fontWeight = FontWeight.SemiBold,
                     textAlign = TextAlign.Center,
                     textDecoration = TextDecoration.Underline
 
