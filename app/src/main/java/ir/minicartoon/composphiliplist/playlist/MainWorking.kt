57f278912ec8acf0f446283013f076bc45936787
miladj796 <miladjamshidpur@gmail.com>
2023-07-28 17:45:00 +0330

    start Part5(stat)

--- app/src/main/java/ir/minicartoon/composphiliplist/playlist/MainWorking.kt
+++ app/src/main/java/ir/minicartoon/composphiliplist/playlist/MainWorking.kt
@@ -3,6 +3,25 @@ package ir.minicartoon.composphiliplist.playlist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainWorking : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +31,5 @@ class MainWorking : ComponentActivity() {
         }
     }
 
-
 }
+
--- app/src/main/java/ir/minicartoon/composphiliplist/playlist/TextStyle.kt
+++ app/src/main/java/ir/minicartoon/composphiliplist/playlist/TextStyle.kt
@@ -1,4 +1,4 @@
-package ir.minicartoon.composphiliplist
+package ir.minicartoon.composphiliplist.playlist
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,21 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import ir.minicartoon.composphiliplist.R
+import ir.minicartoon.composphiliplist.playlist.ui.theme.ComposPhilipListTheme
 
-class MainActivity : ComponentActivity() {
+class TextStyle : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var fontFamily = FontFamily(
@@ -29,9 +33,9 @@ class MainActivity : ComponentActivity() {
             Font(R.font.signikanegative_medium, FontWeight.Medium),
             Font(R.font.signikanegative_semibold, FontWeight.SemiBold),
             Font(R.font.signikanegative_bold, FontWeight.Bold),
-            Font(R.font.signikanegative_light, FontWeight.Light)
 
-        )
+
+            )
         setContent {
             Box(
                 modifier = Modifier
@@ -39,33 +43,14 @@ class MainActivity : ComponentActivity() {
                     .fillMaxSize()
 
             ) {
+
                 Text(
-                    text = buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Magenta,
-                                fontSize = 60.sp
-                            )
-                        ) {
-                            append("J")
-                        }
-                        append("etpack ")
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Magenta,
-                                fontSize = 60.sp
-                            )
-                        ) {
-                            append("C")
-                        }
-                        append("ompose ")
-                    },
-//                    text = "Jetpack Compose",
+                    text = "Jetpack Compose",
                     color = Color.Green,
                     fontSize = 35.sp,
                     fontStyle = FontStyle.Italic,
                     fontFamily = fontFamily,
-                    fontWeight = FontWeight.SemiBold,
+                    fontWeight = FontWeight.Thin,
                     textAlign = TextAlign.Center,
                     textDecoration = TextDecoration.Underline
 
@@ -75,6 +60,5 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
-
 }
+
